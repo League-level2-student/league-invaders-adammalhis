46d60f7f039ab0617b5be066e7523586c25eb511
Adam <Adam@Adam-PC>
2021-09-08 16:56:17 -0700

    I did league invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,27 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
 
+JFrame frame;	
+public static final int Width= 500;
+public static final int Height= 800;
+public static void main(String[] args) {
+	
+	
+	
+	
+	
+	
 }
+
+LeagueInvaders(int JFrame) {
+	frame= new JFrame();
+	
+	}
+
+void setup() {
+	
+	
+}
+}
+//testing
