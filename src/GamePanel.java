f60659ab8b82c1ffc7b37fcf0bfadf8ab120eda9
Adam <Adam@Adam-PC>
2021-09-18 17:58:13 -0700

    I worked on league invaders today.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
@@ -8,7 +10,9 @@ public class GamePanel extends JPanel {
     final int GAME = 1;
     final int END = 2;
     int currentState= MENU;
-    
+    Font titleFont= new Font("Arial", Font.PLAIN, 48);
+    Font enterFont= new Font("Arial", Font.PLAIN, 24);
+
 	@Override
 	public void paintComponent(Graphics g){
 		if(currentState == MENU){
@@ -38,17 +42,24 @@ public class GamePanel extends JPanel {
 	}
 	
 	void drawMenuState(Graphics g) {
-		
-		
-		
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("LEAGUE INVADERS", 15, 250);
+		g.setFont(enterFont);
+		g.drawString("Press ENTER to start", 115, 500);
+		g.drawString("Press SPACE for instructions", 80, 750);
 	}
 	void drawGameState(Graphics g) {
-		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
 		
 		
 	}
 	void drawEndState(Graphics g) {
-		
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
 		
 		
 	}
