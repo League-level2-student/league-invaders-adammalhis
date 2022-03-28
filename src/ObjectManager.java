a8b2fb577f1514a40e3819809b4e91fec2bf7dde
league <league@imac-18.attlocal.net>
2022-03-28 16:51:36 -0700

    I worked on League invaders

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,9 +1,39 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
 	
-	int rocket;
+	Rocketship rocket;
+	ArrayList <Projectile>projectiles = new ArrayList<Projectile>();
+	ArrayList <Alien>aliens = new ArrayList<Alien>();
+	Random random= new Random();
 	
-	ObjectManager(rocketShip) {
+	ObjectManager(Rocketship rocket) {
+		this.rocket = rocket;
+	}
+	void addProjectile(Projectile projectile1) {
+		projectiles.add(projectile1);
+	}
+	void addAlen(Alien alien1) {
+		aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
+	}
+	void update() {
+		for (int i=0; i<aliens.size(); i++) {
+			update();
+			if(aliens.get(i).y <= 800 || aliens.get(i).y >= 0) {
+				aliens.get(i).isActive= false;
+			}
+		}	
+		for (int i=0; i<projectiles.size(); i++) {
+			update();
+			if(projectiles.get(i).y <= 800 || projectiles.get(i).y >= 0) {
+				projectiles.get(i).isActive= false;
+			}
+		}
+	}
+	void draw() {
 		
 	}
+	
+	
 }
